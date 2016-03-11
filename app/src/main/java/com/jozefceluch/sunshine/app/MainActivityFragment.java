92f940f9e2fd7948eb611e6fa2b95510e60d5547
Jozef Celuch <jozef@novoda.com>
2016-03-11 15:00:53 +0000

    Add ListView with fake data

--- app/src/main/java/com/jozefceluch/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/jozefceluch/sunshine/app/MainActivityFragment.java
@@ -1,15 +1,22 @@
 package com.jozefceluch.sunshine.app;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayAdapter<String> forecastAdapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +24,21 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        List<String> forecastItems = new ArrayList<>();
+        forecastItems.add("Today - Sunny - 54/64");
+        forecastItems.add("Tomorrow - Sunny - 54/24");
+        forecastItems.add("Sunday - Raining - 43/64");
+
+        forecastAdapter = new ArrayAdapter<>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                forecastItems
+        );
+        ListView forecastList = (ListView) rootView.findViewById(R.id.listview_forecast);
+        forecastList.setAdapter(forecastAdapter);
+        return rootView;
     }
 }
