2b4bd975dbd2b8e1ea040645f5fc0352d5d2eb16
Jozef Celuch <jozef@novoda.com>
2016-03-13 18:55:26 +0000

    Open settings screen with the SettingsActivity from the course gist

--- app/src/main/java/com/jozefceluch/sunshine/app/MainActivity.java
+++ app/src/main/java/com/jozefceluch/sunshine/app/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            SettingsActivity.showSettings(this);
             return true;
         }
 
